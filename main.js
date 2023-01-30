da294dc11addb14fd0b511d6567d9bfbc94dad64
luishs0 <luishs.doc@gmail.com>
2023-01-30 18:11:47 +0100

    1 e 2

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 
+//Prendo il bottone e il div:
+const btn = document.getElementById("btn");
+const div = document.getElementById("div-1");
+
+// Click del bottone:
+btn.addEventListener("click", function () {
+    toGreen(div);
+});
+
+// Funzione per fare diventare verde lo sfondo di un elemento:
+function toGreen(element) {
+    element.classList.remove("red");
+    element.classList.add("green");
+};
